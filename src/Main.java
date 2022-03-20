f23965ed46cc0a824d6a7144eb50709f6845cceb
molottva <molottva@list.ru>
2022-03-20 14:03:46 +0300

    Type error solved

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int eaters = 5; // сколько людей будут есть
+        int eaters = 3; // сколько людей будут есть
+        int water = 3001; // миллилитров воды
+        int potatoes = 7; // картофелин
+        int chicken = 5; // куриных бёдер
+        int spices = 13; // ложек специй
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        float eatersFloat = (float) eaters;
 
         System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
-
+        System.out.println((water / eatersFloat) + " миллилитров воды");
+        System.out.println((potatoes / eatersFloat) + " картофелин(а)");
+        System.out.println((chicken / eatersFloat) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eatersFloat) + " ложек(ка) специй");
     }
 }
